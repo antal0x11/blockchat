c4bfd995a783ae6149dc955a5314e9e4a8245459
Antonios Alexopoulos <xviia42@gmail.com>
2024-03-15 09:45:22 +0200

    update pos to take into consideration stake as well

--- lib/mine.go
+++ lib/mine.go
@@ -2,13 +2,12 @@ package lib
 
 import (
 	"math/rand"
+	"sort"
 
 	"github.com/antal0x11/blockchat/dst"
 )
 
 func MineBlock(seed *string, neighboors *dst.Neighboors) string {
-
-	// TODO take into consideration the stake of each node
 
 	hashSum := 0
 
@@ -17,7 +16,26 @@ func MineBlock(seed *string, neighboors *dst.Neighboors) string {
 	}
 
 	random := rand.New(rand.NewSource(int64(hashSum)))
-	selected := random.Intn(len(neighboors.DSNodes))
 
-	return neighboors.DSNodes[uint32(selected)].PublicKey
+	// Each node will have n amount of space allocated in the lottery
+	// slice based on the (int) amount of stake it has.
+	// The random number will point the position that each node owns
+	// with its corresponding id in the lottery slice.
+	lottery := make([]int, 0)
+	keysID := make([]int, 0, len(neighboors.DSNodes))
+	for key := range neighboors.DSNodes {
+		keysID = append(keysID, int(key))
+	}
+
+	sort.Ints(keysID)
+
+	for key := range keysID {
+		for range uint(neighboors.DSNodes[uint32(key)].Stake) {
+			lottery = append(lottery, key)
+		}
+	}
+
+	selected := random.Intn(len(lottery))
+
+	return neighboors.DSNodes[uint32(lottery[selected])].PublicKey
 }
